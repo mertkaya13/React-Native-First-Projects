f367cdc6f1c0f840189ca01d7909b3660a98d5bd
Mert Kaya <47145541+mertkaya13@users.noreply.github.com>
2020-04-24 06:10:09 +0300

    Default Names and Changing names for sad-happy app
    Default Names and Changing names for sad-happy app

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import React, {Component ,useState } from 'react';
-import {Text, TextInput , View} from 'react-native';
+import React, {Component, useState} from 'react';
+import {Button, Text, View, Image, TextInput, ScrollView} from 'react-native';
 
-{/*This app translates every word into 3 pizza*/}
-export default function Word3Pizza(){
-  const [text, setText] = useState('');
-  return (
-    <View style = {{padding: 20}}>
-      <TextInput
-        style= {{height: 50}}
-        placeholder="Type words to get 3 pizzas for word."
-        onChangeText={text => setText(text)}
-        defaultValue={text}
-      />
-      <Text style={{padding:10,fontSize:40}}>
-        {text.split(' ').map((word) => word && '🍕🍕🍕').join(' ')}
-      </Text>
-    </View>
-  );
+export class Human extends Component {
+  state = {
+    isLaughing: false,
+    name: this.props.name,
+  };
+  
+  handleName = text => {
+    this.setState({name: text});
+  };
+
+  render(props) {
+    return (
+      <View>
+        <Text>
+          I am {this.state.name}, and I am{' '}
+          {this.state.isLaughing ? 'laughing :D' : 'sad :('}!
+        </Text>
+        <TextInput
+          placeholder="Change My Name???"
+          onChangeText={this.handleName }
+        />
+        <Image source={require('./pp.png')} style={{width: 200, height: 200}} />
+        <Button
+          onPress={() => {
+            if (this.state.isLaughing == false)
+              this.setState({isLaughing: true});
+            else this.setState({isLaughing: false});
+          }}
+          title={this.state.isLaughing ? 'Make him sad' : 'Tickle Tİckle'}
+        />
+      </View>
+    );
+  }
+}
+export default class Cafe extends Component {
+  render() {
+    return (
+      <>
+        <ScrollView>
+          <Human name="Lincoln" />
+          <Human name="Churchill" />
+          <Human name="Ahmet" />
+          <Human name="Mehmet" />
+          <Human name="Ali" />
+          <Human name="Veli" />
+          <Human name="Deli" />
+          <Human name="Mert" />
+          <Human name="Burcu" />
+        </ScrollView>
+      </>
+    );
+  }
 }
