598f0f6436c927500aaa02172129c6b9ef1f0033
Mert Kaya <47145541+mertkaya13@users.noreply.github.com>
2020-04-24 10:59:02 +0300

    FlatListExample
    FlatListExample

--- App.js
+++ App.js
@@ -1,56 +1,43 @@
-import React, {Component, useState} from 'react';
-import {Button, Text, View, Image, TextInput, ScrollView} from 'react-native';
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
-export class Human extends Component {
-  state = {
-    isLaughing: false,
-    name: this.props.name,
-  };
-  
-  handleName = text => {
-    this.setState({name: text});
-  };
+export default (FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+          {key: 'Louie'},
+          {key: 'Johnny'},
+          {key: 'Ali'},
+          {key: 'Veli'},
+          {key: 'Deli'},
+          {key: 'Ahmet'},
+          {key: 'Mehmet'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+});
 
-  render(props) {
-    return (
-      <View>
-        <Text>
-          I am {this.state.name}, and I am{' '}
-          {this.state.isLaughing ? 'laughing :D' : 'sad :('}!
-        </Text>
-        <TextInput
-          placeholder="Change My Name???"
-          onChangeText={this.handleName }
-        />
-        <Image source={require('./pp.png')} style={{width: 200, height: 200}} />
-        <Button
-          onPress={() => {
-            if (this.state.isLaughing == false)
-              this.setState({isLaughing: true});
-            else this.setState({isLaughing: false});
-          }}
-          title={this.state.isLaughing ? 'Make him sad' : 'Tickle Tİckle'}
-        />
-      </View>
-    );
-  }
-}
-export default class Cafe extends Component {
-  render() {
-    return (
-      <>
-        <ScrollView>
-          <Human name="Lincoln" />
-          <Human name="Churchill" />
-          <Human name="Ahmet" />
-          <Human name="Mehmet" />
-          <Human name="Ali" />
-          <Human name="Veli" />
-          <Human name="Deli" />
-          <Human name="Mert" />
-          <Human name="Burcu" />
-        </ScrollView>
-      </>
-    );
-  }
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
